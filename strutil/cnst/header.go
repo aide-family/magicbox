9e24b140769d7d14bc4b2f74ed457b5e0f51a469
aide <1058165620@qq.com>
2025-08-20 10:04:22 +0800

    refactor(server): remove kratos package and update dependencies
    - Remove kratos client and server files
    - Update go.mod to remove unused dependencies
    - Add new cnst package for constants

---
+++ strutil/cnst/header.go
@@ -0,0 +1,21 @@
+// Package cnst provides constants for HTTP headers and security related constants	.
+package cnst
+
+const (
+	HTTPHeaderAcceptLang     = "Accept-Language"
+	HTTHeaderLang            = "Lang"
+	HTTHeaderXRequestID      = "X-Request-ID"
+	HTTHeaderXRealIP         = "X-Real-IP"
+	HTTHeaderXForwardedFor   = "X-Forwarded-For"
+	HTTHeaderXForwardedProto = "X-Forwarded-Proto"
+	HTTPHeaderAuth           = "Authorization"
+	HTTPHeaderXTeamID        = "X-Team-ID"
+	HTTPHeaderXUserID        = "X-User-ID"
+)
+
+const (
+	HTTPHeaderBearerPrefix    = "Bearer "
+	HTTPHeaderContextTypeJSON = "application/json; charset=utf-8"
+	HTTPHeaderContextTypeForm = "application/x-www-form-urlencoded"
+	HTTPHeaderContextTypeText = "text/plain; charset=utf-8"
+)
