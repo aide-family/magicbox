0458584fb8f21baa12592c549823f00fac5c47fc
aide <1058165620@qq.com>
2025-09-12 10:00:27 +0800

    test(password): enhance password unit tests with new password creation validation
    - Added tests to validate the creation of a new password object using the default salt.
    - Ensured that the new password object is not nil and has a non-empty salt.
    - Verified that the new password correctly matches the original password value and is not empty.

--- password/pass_test.go
+++ password/pass_test.go
@@ -16,4 +16,11 @@ func TestNew_DefaultSalt(t *testing.T) {
 	assert.NotEmpty(t, passwordObj.Salt())
 	assert.True(t, passwordObj.Equal(passwordObj.Value()))
 	assert.False(t, passwordObj.Equal(""))
+
+	newPwd, err := password.New(pwd, passwordObj.Salt())
+	assert.Nil(t, err)
+	assert.NotNil(t, newPwd)
+	assert.NotEmpty(t, newPwd.Salt())
+	assert.True(t, newPwd.Equal(passwordObj.Value()))
+	assert.False(t, newPwd.Equal(""))
 }
